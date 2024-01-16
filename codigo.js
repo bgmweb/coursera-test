9855bda2a0e26f670a97d189fd69cecc26b6e7d5
bgmweb <42713790+bgmweb@users.noreply.github.com>
2024-01-16 20:00:31 +0100

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,16 @@
+//condicional para validar algo.
+let nombre = prompt("decime su nombre");
+alert("hola " + nombre);
+
+
+
+if (nombre == "lucas") {
+	alert("tu nombre es malardo")//10 es menor que 15 es true y entonces si se ejecuta.
+}
+
+else if (nombre == "dalto") {
+	alert("tu nombre es " + nombre)
+}
+else {
+	alert("tu nombre es otro")
+}
